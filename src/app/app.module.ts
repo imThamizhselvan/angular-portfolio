5faaea4200a3e4ac23e2159fe19cd62b2454c1eb
Thamizhselvan <thamizhtechie@gmail.com>
2020-05-10 19:57:22 +1000

    adding neomorphism

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    IntroductionComponent,
+    AboutComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
