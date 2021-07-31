49d9d3ff773fb2a64e1a49ec57de7496d71b728b
Thamizhselvan <thamizhtechie@gmail.com>
2021-07-31 17:01:32 +1000

    adding books

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { NewsletterComponent } from './newsletter/newsletter.component';
 import { HomeComponent } from './home/home.component';
 import { PodcastComponent } from './podcast/podcast.component';
+import { BooksComponent } from './books/books.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'newsletter', component: NewsletterComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'podcast', component: PodcastComponent }
+  { path: 'podcast', component: PodcastComponent },
+  { path: 'book', component: BooksComponent }
 ];
 
 @NgModule({
